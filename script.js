3bcd206513df8dd91c811e7e6b11b1a7dfa31558
Ritik <ritikprajapati2704@gmail.com>
2024-07-06 22:14:59 +0530

    updated

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
 
-const scroll= new LocomotiveScroll({
-    el:document.querySelector('.main'),
-    smooth:true
+let tl = gsap.timeline();
+
+tl.to(".to-animate", {
+  y: "100vh",
+  scale: 0.4,
+  duration: 0,
+});
+
+tl.to(".to-animate", {
+  y: "-90vh",
+  delay: 0.5,
+  duration: 1,
+});
+
+tl.to(".to-animate", {
+  y: 0,
+  rotate: "-720deg",
+  delay: 1,
+  duration: 1,
+  scale: 1,
+});
+
+function checkOrientation() {
+  const portraitOverlay = document.getElementById("portrait-overlay");
+
+  if (window.innerHeight > window.innerWidth) {
+    portraitOverlay.style.display = "flex";
+  } else {
+    portraitOverlay.style.display = "none";
+  }
 }
-)
 
-let tl=gsap.timeline()
+window.addEventListener("resize", checkOrientation);
 
-tl.to(".to-animate",{
-    y:'100vh',
-    scale:0.4,
-    duration:0
-})
-
-tl.to(".to-animate",{
-    y:'-90vh',
-    delay:0.5,
-    duration:1
-})
-
-tl.to(".to-animate",{
-    y:0,
-    rotate:'-720deg',
-    delay:1,
-    duration:1,
-    scale:1,
-})
-
-
-
-
+window.addEventListener("load", checkOrientation);
